432f81626c7d8071d65d12948a892dbe3ecc6319
Hassan Dayem <hassandayem91@gmail.com>
2020-07-22 16:49:09 +0300

    added the footer to the home page

--- src/layout/home/index.jsx
+++ src/layout/home/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Footer from '../../components/footer/Footer';
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;
