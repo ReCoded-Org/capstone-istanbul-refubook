89a273c4ce7ea30935458f7d8e4ff8f2564873a4
lilas-saleh <lilas.ahmad.saleh@gmail.com>
2020-08-12 18:30:07 +0300

    fixing errors

--- src/layout/home/index.jsx
+++ src/layout/home/index.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import Testimonial from '../../components/testimonials/Index';
-import Hero from '../../components/hero/Hero';
-import Footer from '../../components/footer/Footer';
+import Navbar from '../../components/navbar/Index';
+import LatestStories from '../../components/latestStories/Index';
+import i18next from 'i18next';
 
-const Home = () => {
+const Home = ({ t }) => {
+  const changeLanguage = (lng) => {
+    i18next.changeLanguage(lng);
+  };
+  const NavbarStyle =
+    'flex flex-col md:flex-row items-center justify-center md:justify-between flex-wrap bg-white p-4';
   return (
     <>
-      <Hero />
-      <Testimonial />
-      <Footer />
+      <Navbar NavbarStyle={NavbarStyle} />
+      <LatestStories />
+      <button onClick={() => changeLanguage('en')}>Eng</button>
+      <button onClick={() => changeLanguage('ar')}>Ar</button>
     </>
   );
 };
-
 export default Home;
