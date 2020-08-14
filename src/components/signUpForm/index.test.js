5020604c1670308b1e4de0efbccbd0199e13ac4d
Aydin <aydindavut14@gmail.com>
2020-08-14 14:07:13 +0300

    fixed test

--- src/components/signUpForm/index.test.js
+++ src/components/signUpForm/index.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import SignUpForm from './Index';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
+import rootReducer from '../../store/reducers/rootReducer';
+const store = createStore(rootReducer, applyMiddleware(logger));
 
 describe('signUpForm', () => {
   test('renders the sign up form component', () => {
-    render(<SignUpForm />);
+    render(
+      <Provider store={store}>
+        <SignUpForm />
+      </Provider>
+    );
   });
 });
